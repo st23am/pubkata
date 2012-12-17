475536ad85d876647804110f488ea2bee1ca790c
James Smith <st23am@gmail.com>
2012-12-17 17:00:20 -0500

    added urinal kata

---
+++ spec/urinal_kata_spec.rb
@@ -0,0 +1,53 @@
+require_relative '../setup'
+require_relative 'spec_helper'
+require_relative '../src/urinal_rules'
+
+describe UrinalRules do
+
+  Given(:urinal_rules) { UrinalRules.new }
+  context "Stalemate (all stalls are full)" do
+    When(:result) { urinal_rules.solve([true, true, true, true], false) }
+    Then { result.should be_nil } #all stalls are occupied get out now.
+  end
+
+  context "Rule 1: get as far away from the door as possible" do
+    When(:result) { urinal_rules.solve([false, false, false,false, false], false) }
+    Then { result.should == 4 }
+  end
+
+  context "Rule 2: Avoid standing next to another dude" do
+    When(:result) { urinal_rules.solve([true, false, false, false, true], true) }
+    Then { result.should == 2 }
+  end
+
+  context "Rule 2: Exit to in order to avoid standing next to another dude" do
+    When(:result) { urinal_rules.solve([true, false, true, false, true], false) }
+    Then { result.should be_nil }
+  end
+
+  context "Rule 2, Exception 1: you can havea  neighbor if there is a line" do
+    When(:result) { urinal_rules.solve([false, true, false, true, false], true) }
+    Then { result.should == 4 }
+  end
+
+  context "Rule 2 Exception 2: You can have a neighbor if someone else already does." do
+    When(:result) { urinal_rules.solve([true, true, false, false, true], false) }
+    Then { result.should == 3 }
+  end
+
+  context "Rule 3: Really avoid standing next to TWO other dudes." do
+    When(:result) { urinal_rules.solve([true, false, true, false, true], false) }
+    Then { result.should be_nil }
+  end
+
+  context "Rule 3: Exception 1: You can have two neighbors if there is a line" do
+    When(:result) { urinal_rules.solve([true, false, true, false, true], true) }
+    Then { result.should == 3 }
+  end
+
+  context "Rule 3: Exception 1: You can have two neighbors if somoene else already does" do
+    When(:result) { urinal_rules.solve([true, true, true, false, true], false) }
+    Then { result.should == 3 }
+  end
+end
+
---
+++ src/urinal_rules.rb
@@ -0,0 +1,7 @@
+require_relative '../setup'
+
+class UrinalRules
+  def solve(situation=[], line_status)
+
+  end
+end
